bc70fc5212f7cf2a196bd309b6b525b736337bef
LuisCeballosH <luisceballoshernandez@gmail.com>
2020-07-26 12:43:19 -0500

    sección 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,40 @@
 import express from 'express';
 import { serverPort } from '../global/environment';
+import io from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/sockets';
+
 
 export default class Server {
 
     public app: express.Application;
     public port: number;
+    public io: io.Server;
+    private httpServer: http.Server;
+    private static _instance: Server;
 
-    constructor() {
+    private constructor() {
         this.app = express();
         this.port = serverPort;
+        this.httpServer = new http.Server(this.app);
+        this.io = io(this.httpServer);
+        this.hearSocket();
+    }
+
+    public static getInstance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    private hearSocket() {
+        console.log('hearSocket');
+        this.io.on('connection', client => {
+            console.log('connection client');
+            socket.disconnect(client);
+            socket.message(client, this.io);
+        });
     }
 
     start(callback: any) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
